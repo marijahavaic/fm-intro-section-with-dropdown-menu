30fdc78aa7e9b226f5511983c6c5e3e3e0a7536e
Marija Havaic <marija.havaic@gmail.com>
2022-12-03 14:41:13 +0100

    Style right side of the navbar

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const dropdownBtn = document.querySelector(".dropbtn");
+const arrowUp = document.getElementById("arrowUp");
+const arrowDown = document.getElementById("arrowDown");
 
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
@@ -6,11 +8,18 @@ function myFunction() {
     document.getElementById("myDropdown").classList.toggle("show");
 }
 
-function arrowToggle() {
-    document.getElementById("arrow").classList.toggle("show");
+function setArrowUp() {
+    arrowDown.classList.remove("show");
+    arrowUp.classList.add("show");
 }
 
-dropdownBtn.addEventListener("onHover", arrowToggle());
+function setArrowDown() {
+    arrowDown.classList.add("show");
+    arrowUp.classList.remove("show");
+}
+
+dropdownBtn.addEventListener("mouseover", setArrowUp);
+dropdownBtn.addEventListener("mouseout", setArrowDown);
 
 // Close the dropdown menu if the user clicks outside of it
 window.onclick = function (event) {
