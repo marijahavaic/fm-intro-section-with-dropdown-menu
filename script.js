d08a98e6e48ae852355d4775bc5b29b1dc13d90b
Marija Havaic <marija.havaic@gmail.com>
2022-12-03 18:07:13 +0100

    Adjust the mobile version

--- script.js
+++ script.js
@@ -4,10 +4,6 @@ const arrowDown = document.getElementById("arrowDown");
 
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-}
-
 function setArrowUp() {
     arrowDown.classList.remove("show");
     arrowUp.classList.add("show");
@@ -21,6 +17,17 @@ function setArrowDown() {
 dropdownBtn.addEventListener("mouseover", setArrowUp);
 dropdownBtn.addEventListener("mouseout", setArrowDown);
 
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+function toggleResponsiveness() {
+    var x = document.getElementById("navbar");
+    var y = document.getElementById("dropdown");
+    if (x.className === "navbar") {
+        x.className += " responsive";
+        y.className += "responsive";
+    } else {
+        x.className = "navbar";
+    }
+}
 // Close the dropdown menu if the user clicks outside of it
 window.onclick = function (event) {
     if (!event.target.matches(".dropbtn")) {
