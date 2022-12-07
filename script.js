581bb8cf613e4a19182dad6231465c279a73d96b
Marija Havaic <marija.havaic@gmail.com>
2022-12-07 16:15:08 +0100

    Fix mobile version layout

--- script.js
+++ script.js
@@ -18,16 +18,23 @@ dropdownBtn.addEventListener("mouseover", setArrowUp);
 dropdownBtn.addEventListener("mouseout", setArrowDown);
 
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-function toggleResponsiveness() {
-    var x = document.getElementById("navbar");
-    var y = document.getElementById("dropdown");
-    if (x.className === "navbar") {
-        x.className += " responsive";
-        y.className += "responsive";
-    } else {
-        x.className = "navbar";
-    }
-}
+// function toggleResponsiveness() {
+//     var x = document.getElementById("sidepanel");
+//     var y = document.querySelectorAll(".dropdown");
+//     var z = document.getElementById("navbar");
+//     var overlay = document.getElementsByClassName("overlay");
+//     if (x.className === "sidepanel") {
+//         x.className += " responsive";
+//         y.className += " responsive";
+//         z.className += " responsive";
+//         overlay.className = " active";
+//     } else {
+//         x.className = "sidepanel";
+//         y.className = "dropdown";
+//         z.className += "navbar";
+//         overlay.className = "overlay";
+//     }
+// }
 // Close the dropdown menu if the user clicks outside of it
 window.onclick = function (event) {
     if (!event.target.matches(".dropbtn")) {
@@ -41,3 +48,13 @@ window.onclick = function (event) {
         }
     }
 };
+
+/* Set the width of the sidebar to 250px (show it) */
+function openNav() {
+    document.getElementById("sidepanel").style.width = "250px";
+}
+
+/* Set the width of the sidebar to 0 (hide it) */
+function closeNav() {
+    document.getElementById("sidepanel").style.width = "0";
+}
