4f580c43f5167545ccdc1b216e1650c47e6e91e4
Marija Havaic <marija.havaic@gmail.com>
2022-12-07 16:53:14 +0100

    Add sidepanel and overlay

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const dropdownBtn = document.querySelector(".dropbtn");
 const arrowUp = document.getElementById("arrowUp");
 const arrowDown = document.getElementById("arrowDown");
+const overlay = document.getElementById("overlay");
 
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
@@ -18,23 +19,23 @@ dropdownBtn.addEventListener("mouseover", setArrowUp);
 dropdownBtn.addEventListener("mouseout", setArrowDown);
 
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
-// function toggleResponsiveness() {
-//     var x = document.getElementById("sidepanel");
-//     var y = document.querySelectorAll(".dropdown");
-//     var z = document.getElementById("navbar");
-//     var overlay = document.getElementsByClassName("overlay");
-//     if (x.className === "sidepanel") {
-//         x.className += " responsive";
-//         y.className += " responsive";
-//         z.className += " responsive";
-//         overlay.className = " active";
-//     } else {
-//         x.className = "sidepanel";
-//         y.className = "dropdown";
-//         z.className += "navbar";
-//         overlay.className = "overlay";
-//     }
-// }
+function toggleResponsiveness() {
+    var x = document.getElementById("sidepanel");
+    var y = document.querySelectorAll(".dropdown");
+    var z = document.getElementById("navbar");
+    var overlay = document.getElementsByClassName("overlay");
+    if (x.className === "sidepanel") {
+        x.className += " responsive";
+        y.className += " responsive";
+        z.className += " responsive";
+        overlay.className = " active";
+    } else {
+        x.className = "sidepanel";
+        y.className = "dropdown";
+        z.className += "navbar";
+        overlay.className = "overlay";
+    }
+}
 // Close the dropdown menu if the user clicks outside of it
 window.onclick = function (event) {
     if (!event.target.matches(".dropbtn")) {
@@ -52,9 +53,13 @@ window.onclick = function (event) {
 /* Set the width of the sidebar to 250px (show it) */
 function openNav() {
     document.getElementById("sidepanel").style.width = "250px";
+    overlay.style.visibility = "visible";
+    overlay.style.opacity = 1;
 }
 
 /* Set the width of the sidebar to 0 (hide it) */
 function closeNav() {
     document.getElementById("sidepanel").style.width = "0";
+    overlay.style.visibility = "hidden";
+    overlay.style.opacity = 0;
 }
