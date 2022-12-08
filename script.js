80fbfa144b3eb3ae6dd732ecd1e144d7cf9aa713
Marija Havaic <marija.havaic@gmail.com>
2022-12-08 17:11:59 +0100

    Change readme

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-const dropdownBtn = document.querySelector(".dropbtn");
+const featuresDropBtn = document.getElementById("featuresDropBtn");
+const companyDropBtn = document.getElementById("companyDropBtn");
 const arrowUp = document.getElementById("arrowUp");
 const arrowDown = document.getElementById("arrowDown");
 const overlay = document.getElementById("overlay");
+const dropDownContent = document.querySelectorAll(".dropdown-content");
 
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
@@ -15,22 +17,47 @@ function setArrowDown() {
     arrowUp.classList.remove("show");
 }
 
-function toggleArrow() {
-    if (arrowDown.className === "arrowDown show") {
-        arrowDown.classList.remove("show");
-        arrowUp.classList.add("show");
-    } // else {
-    //     arrowUp.classList.remove("show");
-    //     arrowDown.classList.add("show");
-    // }
+// Close the dropdown menu if the user clicks outside of it
+function closeDropdown(event) {
+    if (!event.target.matches(".dropbtn")) {
+        var dropdowns = document.getElementsByClassName("dropdown-content");
+        var i;
+        for (i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains("show")) {
+                openDropdown.classList.remove("show");
+            }
+        }
+    }
+}
+
+function toggleOpen() {
+    dropDownContent.style.display === "block" ? "none" : "block";
+}
+
+/* Set the width of the sidebar to 250px (show it) */
+function openNav() {
+    document.getElementById("sidepanel").style.width = "250px";
+    overlay.style.visibility = "visible";
+    overlay.style.opacity = 1;
+}
+
+/* Set the width of the sidebar to 0 (hide it) */
+function closeNav() {
+    document.getElementById("sidepanel").style.width = "0";
+    overlay.style.visibility = "hidden";
+    overlay.style.opacity = 0;
 }
 
 // Check the screen sizes for different mouse events
 if (window.screen.width > 600) {
-    dropdownBtn.addEventListener("mouseover", setArrowUp);
-    dropdownBtn.addEventListener("mouseout", setArrowDown);
+    featuresDropBtn.addEventListener("mouseover", setArrowUp);
+    featuresDropBtn.addEventListener("mouseout", setArrowDown);
+    companyDropBtn.addEventListener("mouseover", setArrowUp);
+    companyDropBtn.addEventListener("mouseout", setArrowDown);
+    window.addEventListener("click", closeDropdown);
 } else {
-    dropdownBtn.addEventListener("click", toggleArrow);
+    featuresDropBtn.addEventListener("click", toggleOpen);
 }
 
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
@@ -50,31 +77,4 @@ function toggleResponsiveness() {
         z.className += "navbar";
         overlay.className = "overlay";
     }
-}
-// Close the dropdown menu if the user clicks outside of it
-window.onclick = function (event) {
-    if (!event.target.matches(".dropbtn")) {
-        var dropdowns = document.getElementsByClassName("dropdown-content");
-        var i;
-        for (i = 0; i < dropdowns.length; i++) {
-            var openDropdown = dropdowns[i];
-            if (openDropdown.classList.contains("show")) {
-                openDropdown.classList.remove("show");
-            }
-        }
-    }
-};
-
-/* Set the width of the sidebar to 250px (show it) */
-function openNav() {
-    document.getElementById("sidepanel").style.width = "250px";
-    overlay.style.visibility = "visible";
-    overlay.style.opacity = 1;
-}
-
-/* Set the width of the sidebar to 0 (hide it) */
-function closeNav() {
-    document.getElementById("sidepanel").style.width = "0";
-    overlay.style.visibility = "hidden";
-    overlay.style.opacity = 0;
 }
